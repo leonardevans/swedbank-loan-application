6fa546c137f89de05c52b79eb5d95bbad4920a50
Leo Mutugi <leo@kali.bedubiz.com>
2021-12-27 21:51:50 +0000

    Updates

---
+++ precache-manifest.281be2341bd55388989c178486e10506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "666598b2091f4c4b079c",
+    "url": "/leonardevans/swedbank-loan-application/static/css/main.60b4022b.chunk.css"
+  },
+  {
+    "revision": "666598b2091f4c4b079c",
+    "url": "/leonardevans/swedbank-loan-application/static/js/main.5cd3e2f0.chunk.js"
+  },
+  {
+    "revision": "bafcb331804a6941c0ec",
+    "url": "/leonardevans/swedbank-loan-application/static/js/runtime~main.bc9120c4.js"
+  },
+  {
+    "revision": "29a416c4904b2bed9309",
+    "url": "/leonardevans/swedbank-loan-application/static/js/2.123b40ef.chunk.js"
+  },
+  {
+    "revision": "f6e7b3967ccff186db8c",
+    "url": "/leonardevans/swedbank-loan-application/static/js/3.f7ed5ee6.chunk.js"
+  },
+  {
+    "revision": "aac1521fd080c21d4e070a307c4d495f",
+    "url": "/leonardevans/swedbank-loan-application/static/media/swedbank-logo.aac1521f.svg"
+  },
+  {
+    "revision": "867c83c748d3cdfa7daf6c1f0f9cc027",
+    "url": "/leonardevans/swedbank-loan-application/index.html"
+  }
+];
